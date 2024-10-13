ecf1cadda0e93cfac2df2fdc3f9e80c568bf6813
xingyu yao <xy2779@nyu.edu>
2024-10-13 16:40:44 -0400

    readme

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,64 +1,70 @@
-let snowflakes = []; // 存储所有的雪花
-let coverProgress = 0; // 覆盖的进度
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255, 180, 255);
 }
+
+let xPeriodSec = 8;
+let rPeriodSec = 1;
+let yPeriodSec = 0.8;
+let xSpeed = 5;
+let xposC = 0;
 
 function draw() {
-  background(0); // 设置背景为黑色，雪花为白色
+  background(0);
+  //this ellipse represent the old
+  let yA = height / 8;
+  fill("darkred");
+  let xposA = 4 * frameCount;
+  ellipse(xposA % width, yA, 25);
 
-  // 生成新的雪花
-  let t = frameCount / 60; // 更新时间
-  for (let i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // 在随机位置生成雪花
-  }
+  //this ellipse represent the excitement
+  let xAmplitude = width;
+  let xOffset = 0;
+  let xPeriod = xPeriodSec * getTargetFrameRate();
 
-  // 绘制所有雪花
-  for (let flake of snowflakes) {
-    flake.update(t); // 更新雪花的位置
-    flake.display(); // 显示雪花
-  }
+  let rAmplitude = 150;
+  let rOffset = 10;
+  let rPeriod = rPeriodSec * getTargetFrameRate();
 
-  // 覆盖进度条，模拟雪逐渐覆盖
-  fill(255, 255, 255, 150);
-  noStroke();
-  rect(0, 0, windowWidth, windowHeight * coverProgress); // 进度条逐渐填充
+  let xB = xAmplitude * (sin((TWO_PI / xPeriod) * frameCount) + 1) * 0.5;
 
-  // 当覆盖进度达到 100%，停止动画
-  if (coverProgress >= 1) {
-    noLoop(); // 停止 draw() 的循环
+  let yB = (height * 2) / 6;
+
+  let r = rAmplitude * (sin((TWO_PI / rPeriod) * frameCount) + 1) * 0.5;
+  fill("red");
+  ellipse(xB + xOffset, yB, r + rOffset);
+
+  //this ellipse represent doubt
+  let xPeriodSecD = 0.5;
+  let yPeriodSecD = 0.5;
+  let xPeriodD = xPeriodSecD * getTargetFrameRate();
+  let xD = xAmplitude * (sin((TWO_PI / xPeriod) * frameCount) + 1) * 0.5;
+
+  let yAmplitude = height / 6;
+  let yOffsetD = (height * 4) / 6;
+
+  let yPeriodD = yPeriodSecD * getTargetFrameRate();
+  let yD = yAmplitude * (cos((TWO_PI / yPeriodD) * frameCount) + 1) * 0.5;
+  fill("BlueViolet");
+  ellipse(xD + xOffset, yD + yOffsetD, 100);
+
+  //this ellipse represent the young
+  let xC = xposC; // Let x position increment with time
+
+  let yOffsetC = (height * 3) / 6;
+
+  // Vertical Oscillation
+  let yPeriod = yPeriodSec * getTargetFrameRate();
+  let yC = yAmplitude * (sin((TWO_PI / yPeriod) * frameCount) + 1) * 0.5;
+
+  fill("pink");
+  ellipse(xC, yC + yOffsetC, 40);
+
+  // As each frame increases, the realization moves to the right
+  xposC += xSpeed;
+
+  // Reset xposC back to the initial position when the circle is out of the screen
+  if (xposC > width) {
+    xposC = 0;
   }
 }
-
-// 雪花类
-function snowflake() {
-  this.posX = random(0, width);
-  this.posY = random(-50, 0);
-  this.size = random(2, 5);
-  this.speed = random(0.5, 3);
-
-  this.update = function(time) {
-    this.posY += this.speed; // 雪花往下落
-    // 重置雪花位置到顶部
-    if (this.posY > height) {
-      this.posY = random(-50, 0);
-    }
-  };
-
-  this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
-  };
-}
-
-// 模拟与他人对话并逐渐停止的过程
-function mousePressed() {
-  // 每次点击，覆盖的进度增加
-  coverProgress += 0.1;
-  if (coverProgress >= 1) {
-    // 覆盖完成后停止对话
-    noLoop();
-  }
-}
-
-
--- README.md
+++ README.md
@@ -1,64 +1,53 @@
-# p5.js multi sketch template
+# HW05A
 
-By putting different sketch.js and index.html files in directories we can more easily switch between multiple experiments.
+## Clock Star Animation
 
----
+This project is a clock-inspired animation that visualizes time using geometric shapes. The clock is represented by rotating objects corresponding to seconds, minutes, and hours.
 
-This is a README file that can be used to describe and document your assignment.
+## How It Works
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+- **Seconds**: A star shape rotates with the progression of seconds.
+- **Minutes**: A triangle rotates with the progression of minutes.
+- **Hours**: A long, rounded rectangle rotates to represent the passing hours.
+- The current time is determined using the `millis()` function to calculate seconds, minutes, and hours.
 
----
----
+## problem I met in the process
+I was trying to fill the stars with color and found that I couldn't do it
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
 
-**bold text**
+![image1](96572cba3a8ae74f5d79bfe00d1bc02.png)
 
-*italicized text*
+# HW05B
 
-~~strikethrough text~~
+I chose one of my favorite pieces about listening from last week's READING:
+**BODY SOUND TAPE PIECE**
+**Make body sound tapes of differentpeople at different times.**
+**Of the old, young, crying, longing,excited, calm, doubtful, etc.**
+**1964 spring**
 
-Ordered List:
-1. First item
-2. Second item
-3. Third item
+[Reference](https://idmp5.github.io/creative-coding/cycles/)
+[Reference](https://idmp5.github.io/creative-coding/sincos/)
+[Reference](https://p5js.org/zh-Hans/reference/p5/getTargetFrameRate/)
 
-Unordered List:
-- First item
-- Second item
-- Third item
+## the old
 
-`short code block`
+
+![the old](image.png)
+use dark red color to express the feeling of old age
 
 ```
-extended code block
-fun() {
-  return 0
-}
+  let yA = height / 8;
+  fill("darkred");
+  let xposA = 4 * frameCount;
+  ellipse(xposA % width, yA, 25);
 ```
 
-Link:  
-[linked text](https://www.example.com)
+## the young
+use pink color
+Rhythmic vibration with moderate speed
 
+## excitement
+feeling like a beating heart
 
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+## doubt
+The color purple has a mysterious feel
